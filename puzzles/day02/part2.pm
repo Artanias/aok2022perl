ad9c895eca1f0d03ee4e72d253a6307169cf56cf
Artanias <awa.ba@yandex.ru>
2022-12-08 10:35:40 +0300

    Added solution for the second day part two.

---
+++ puzzles/day02/part2.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+
+my $filename = "data.txt";
+my $win_rules = {
+	rock => "scissors",
+	paper => "rock",
+	scissors => "paper",
+};
+my $draw_rules = {
+	rock => "rock",
+	paper => "paper",
+	scissors => "scissors",
+};
+my $lose_rules = {
+	rock => "paper",
+	paper => "scissors",
+	scissors => "rock",
+};
+my $cipher = {
+	A => "rock",
+	X => $win_rules,
+	B => "paper",
+	Y => $draw_rules,
+	C => "scissors",
+	Z => $lose_rules
+};
+my $choice_score = {
+	rock => 1,
+	paper => 2,
+	scissors => 3
+};
+my $handle;
+
+open($handle, "< :encoding(UTF-8)", $filename)
+	|| die "$0: can't open $filename for reading: $!";
+
+my $score = 0;
+while (<$handle>) {
+	my @choices = split " ", $_;
+	
+	my $enemy_choice = $cipher->{$choices[0]};
+        my $your_choice = $cipher->{$choices[1]}{$enemy_choice};
+
+	my $you_win = $win_rules->{$your_choice};
+
+	$score += $choice_score->{$your_choice};
+	if ($you_win eq $enemy_choice) {
+		$score += 6;
+	}
+	elsif ($enemy_choice eq $your_choice) {
+		$score += 3;
+	}
+}
+
+say "Your total score is $score.";
